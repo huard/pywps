478d9c071b71fa97154d851f7f602116f9c1cb5b
David Huard <david.huard@gmail.com>
2018-06-21 13:13:57 -0400

    replaced import errors by warnings for validation dependencies

--- pywps/dependencies.py
+++ pywps/dependencies.py
@@ -3,13 +3,17 @@
 # licensed under MIT, Please consult LICENSE.txt for details     #
 ##################################################################
 from pywps.exceptions import NoApplicableCode
+import warnings
 
+# It seems these should raise warnings, not errors.
 try:
     from osgeo import gdal, ogr
 except ImportError as err:
-    raise NoApplicableCode('Complex validation requires GDAL/OGR support.')
+    warnings.warn('Complex validation requires GDAL/OGR support.')
+    #raise NoApplicableCode('Complex validation requires GDAL/OGR support.')
 
 try:
     import netCDF4
 except ImportError as err:
-    raise NoApplicableCode('Complex validation requires netCDF4 support.')
+    warnings.warn('Complex validation requires netCDF4 support.')
+    #raise NoApplicableCode('Complex validation requires netCDF4 support.')
